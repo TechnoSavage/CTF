214110cd1edc032303aa58b047754064d62db5a8
TechnoSavage <technosavage@gmail.com>
2023-12-22 09:12:08 -0500

    .

--- HTB/htb_babyencryption.py
+++ HTB/htb_babyencryption.py
@@ -12,23 +12,24 @@
 # f.write(ct.hex())
 # f.close()
 
-import string
+def findKey(cipher):
+    key= []
+    for char in cipher:
+        k = 0
+        while ((256 * k + char - 18) / 123) % 1 != 0:
+            k += 1
+        key.append(k)
+    return(key)
 
-def decryption(hex):
-    #convert from hex to bytes
-    bytes = bytearray.fromhex(hex)
-    #holder for plain text 
+def decryption(cipher):
     pt = []
-    #reversing match minus modulo function (modulo 256 wraps any integer above 256 resulting in difference between original value and 256)
-    for char in bytes:
-        value = ((char - 18) / 123) #Only two values will ever NOT have modulo applied: 1 and 2
-        #accounting for modulo function
-        if value >= 256:
-            value = value - 256
-        pt.append(value)
-    #convert byte array to ascii/utf? string
-    return ascii(pt)
+    key = findKey(cipher)
+    for k in range(len(key)):
+        pt.append((256 * key[k] + cipher[k] - 18) / 123)
+    return(pt)
 
-#hex = "6e0a9372ec49a3f6930ed8723f9df6f6720ed8d89dc4937222ec7214d89d1e0e352ce0aa6ec82bf622227bb70e7fb7352249b7d893c493d8539dec8fb7935d490e7f9d22ec89b7a322ec8fd80e7f8921"
-hex = "8d0883fe79f46fea658d0883fe79f46fea658d0883fe79f46fea65" #known string: 123456789123456789123456789
-print(decryption(hex))
+# with open ('msg.enc', 'r') as f:
+#     cipher = bytes.fromhex(f.read())
+cipher = bytes.fromhex('6e0a9372ec49a3f6930ed8723f9df6f6720ed8d89dc4937222ec7214d89d1e0e352ce0aa6ec82bf622227bb70e7fb7352249b7d893c493d8539dec8fb7935d490e7f9d22ec89b7a322ec8fd80e7f8921')
+pt = decryption(cipher)
+print(''.join([chr(int(char)) for char in pt]))
